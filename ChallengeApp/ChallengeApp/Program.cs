c69d535335614f5fd82ce2762c7c795467693961
dysan66 <dysan66@op.pl>
2023-03-16 17:00:47 +0100

    Zadanie domowe do lekcji 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var adge1 = 15;
+var adge2 = 30;
+var adge3 = 33;
+string name1 = "Ewa";
+string name2 = "Jan";
+bool kobieta = true;
+string wynik1 = "Kobieta ponizej 30 lat";
+string wynik2 = "Ewa, lat 33";
+string wynik3 = "Niepelnoletni Mezczyzna";
+if (kobieta)
+{
+    if (adge2 > 30 && name1 == "Ewa")
+    {
+        if (adge3 == 34) ;
+    }
+    if (name2 == "Jan") ;
+    {
+        if (adge1 < 18) ;
+        {
+            Console.WriteLine(wynik3);
+        }
+        }
+    }
