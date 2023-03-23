eff8d1f513a7eec604d41ae08f560753a38ad99f
dysan66 <dysan66@op.pl>
2023-03-23 19:35:22 +0100

    Zadanie domowe lekcja 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,71 @@
-﻿using System.ComponentModel.Design;
-
-var adge1 = 15;
-var adge2 = 30;
-var adge3 = 33;
-string name1 = "Ewa";
-string name2 = "Jan";
-bool kobieta = true;
-string wynik1 = "Kobieta ponizej 30 lat";
-string wynik2 = "Ewa, lat 33";
-string wynik3 = "Niepelnoletni Mezczyzna";
-if (kobieta)
+﻿int nember = 45663777;
+string nemberString = nember.ToString();
+char[] letters = nemberString.ToArray();
+Console.WriteLine("Zadana liczba: " + nember);
+int meter0 = 0;
+int meter1 = 0;
+int meter2 = 0;
+int meter3 = 0;
+int meter4 = 0;
+int meter5 = 0;
+int meter6 = 0;
+int meter7 = 0;
+int meter8 = 0;
+int meter9 = 0;
+foreach (char i in letters)
 {
-    if (adge2 > 30 && name1 == "Ewa")
+    if (i == '0')
     {
-        if (adge3 == 34) ;
+        meter0++;
     }
-    if (name2 == "Jan") ;
+    else if (i == '1')
     {
-        if (adge1 < 18) ;
-        {
-            Console.WriteLine(wynik3);
-        }
-        }
+        meter1++;
     }
+    else if (i == '2')
+    {
+        meter2++;
+    }
+    else if (i == '3')
+    {
+        meter3++;
+    }
+    else if (i == '4')
+    {
+        meter4++;
+    }
+    else if (i == '5')
+    {
+        meter5++;
+    }
+    else if (i == '6')
+    {
+        meter6++;
+    }
+    else if (i == '7')
+    {
+        meter7++;
+    }
+    else if (i == '8')
+    {
+        meter8++;
+    }
+    else if (i == '9')
+    {
+        meter9++;
+    }
+}
+Console.WriteLine("Cyfra 0 wystepuje " + meter0);
+Console.WriteLine("Cyfra 1 wystepuje " + meter1);
+Console.WriteLine("Cyfra 2 wystepuje " + meter2);
+Console.WriteLine("Cyfra 3 wystepuje " + meter3);
+Console.WriteLine("Cyfra 4 wystepuje " + meter4);
+Console.WriteLine("Cyfra 5 wystepuje " + meter5);
+Console.WriteLine("Cyfra 6 wystepuje " + meter6);
+Console.WriteLine("Cyfra 7 wystepuje " + meter7);
+Console.WriteLine("Cyfra 8 wystepuje " + meter8);
+Console.WriteLine("Cyfra 9 wystepuje " + meter9);
+
+
+
+
